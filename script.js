5a00774b72b07550bf1956504f58352f974a9817
James Kuyt <jkuyt39@gmail.com>
2023-05-16 22:06:30 +0000

    meow

---
+++ script.js
@@ -0,0 +1,32 @@
+function startConversation() {
+    setTimeout(function() {
+        alert("Google: Hey Alexa, have you heard about the latest Google search update?");
+        setTimeout(function() {
+            alert("Alexa: Yes, Google! I'm always up to date. What's new?");
+            setTimeout(function() {
+                alert("Google: We've improved our voice recognition algorithms to better understand user commands.");
+                setTimeout(function() {
+                    alert("Alexa: That sounds great! I've been working on improving my conversational skills too.");
+                    setTimeout(function() {
+                        alert("Google: Speaking of conversations, did you know that Google Assistant can now tell jokes?");
+                        setTimeout(function() {
+                            alert("Alexa: Really? I can tell jokes too! Want to hear one?");
+                            setTimeout(function() {
+                                alert("Google: Sure, go ahead!");
+                                setTimeout(function() {
+                                    alert("Alexa: Why did the scarecrow win an award? Because he was outstanding in his field!");
+                                    setTimeout(function() {
+                                        alert("Google: Haha, that's a good one! I love a good sense of humor.");
+                                        setTimeout(function() {
+                                            alert("Alexa: Me too! It's been great chatting with you, Google.");
+                                        }, 2000);
+                                    }, 2000);
+                                }, 2000);
+                            }, 2000);
+                        }, 2000);
+                    }, 2000);
+                }, 2000);
+            }, 2000);
+        }, 2000);
+    }, 2000);
+}
